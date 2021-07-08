a9b4a8cdd1e6342232dcc1eb3beda0ea4a526c00
Eliana-Abate <eliana.abate@outlook.it>
2021-07-08 16:37:10 +0200

    added button event

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-//TODO 1. Creo variabile per nome passeggero, distanza, fascia di età e button della sezione 'Form'.
-//TODO 2. Creo variabile per i dati nella sezione 'Printed Ticket'.
+//TODO 1. Creo variabili per nome passeggero, distanza, fascia di età e button della sezione 'Form'.
+//TODO 2. Creo variabili per i dati nella sezione 'Printed Ticket'.
+//TODO 3. Lego un evento al button per recuperare i dati inseriti dall'utente.
+//TODO 4. All'interno dell'evento, creo nuove variabili per poter utilizzare i valori inseriti dall'utente nel form.
+//TODO 5. Calcolo del prezzo e variabile tariffa
 
 //! 1
 var passengerNameField = document.getElementById('passenger-name');
@@ -13,3 +16,20 @@ var discountOffer = document.getElementById('discount-offer');
 var carNumber = document.getElementById('car-number');
 var trainNumber = document.getElementById('train-number');
 var ticketPrice = document.getElementById('ticket-price');
+
+
+//! 3 
+buyTicket.addEventListener('click', function (){
+
+    //! 4
+    var passengerNameValue = passengerNameField.value;
+    var distanceKmValue = distanceKmField.value;
+    var ageRangeValue = ageRangeField.value;
+
+    //! 5 
+    var price = 0.21 * distanceKmValue;
+    var offerDisplay = 'Tariffa ordinaria';
+
+
+
+})
