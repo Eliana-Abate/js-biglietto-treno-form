004c071e4950daa199e1cef5b27b11cd033a79b8
Eliana-Abate <eliana.abate@outlook.it>
2021-07-08 16:14:40 +0200

    added var for section data

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//TODO 1. Creo variabile per nome passeggero, distanza, fascia di età e button della sezione 'Form'.
+//TODO 2. Creo variabile per i dati nella sezione 'Printed Ticket'.
+
+//! 1
+var passengerNameField = document.getElementById('passenger-name');
+var distanceKmField = document.getElementById('distance-km');
+var ageRangeField = document.getElementById('age-range');
+var buyTicket = document.getElementById('buy-ticket');
+
+//! 2
+var passengerNameTicket = document.getElementById('passenger-name-ticket');
+var discountOffer = document.getElementById('discount-offer');
+var carNumber = document.getElementById('car-number');
+var trainNumber = document.getElementById('train-number');
+var ticketPrice = document.getElementById('ticket-price');
